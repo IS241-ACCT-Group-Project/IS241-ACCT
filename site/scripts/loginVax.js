304b1d473d315d61437742a591d83efe8cf67064
Kathy Strebel <kthstrebel4@gmail.com>
2022-03-22 17:07:29 -0500

    Add success notification to successful form submission
    4QWY2

--- site/scripts/loginVax.js
+++ site/scripts/loginVax.js
@@ -8,7 +8,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "XiomaraAdmin" && password === "somethingsafe") {
+    if ((username === "XiomaraAdmin" || username === "SurveyUser") && password === "somethingsafe") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
