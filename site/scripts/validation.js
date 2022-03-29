4ffa1ae318d8eb10603be720b894a1d60650041e
Xiomara Mendoza <mendozacolonx@gmail.com>
2022-03-29 17:54:55 -0500

    Update validation.js

--- site/scripts/validation.js
+++ site/scripts/validation.js
@@ -2,6 +2,10 @@ const name = document.getElementById('name')
 const address = document.getElementById('address')
 const zipCode = document.getElementById('zipCode')
 const phone = document.getElementById('phone')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const siteID = document.getElementByID('siteID')
+const patientID = document.getElementById('paitentID')
 
 var form = document.getElementById("newSiteForm");
 form.addEventListener('submit', (e) => {
@@ -16,6 +20,20 @@ form.addEventListener('submit', (e) => {
 		messages.push("Zipcode must be 5 digits.")
 	}
 
+	if (username.value === '' || username.value == null) {
+		messages.push('Username is required')
+	}
+
+	if (password.value === '' || password.value == null) {
+		messages.push('Password is required')
+	}
+	if (siteID.value === '' || siteID.value == null) {
+		messages.push('Site ID is required')
+	}
+	if (patientID.value === '' || paiteintID.value == null) {
+		messages.push('Patient ID is required')
+	}
+
 	function phone(inputtxt) {
 		var phoneno = /^\d{10}$/;
 		if ((inputtxt.value.match(phoneno))
@@ -26,12 +44,6 @@ form.addEventListener('submit', (e) => {
 			alert("Please enter a valid 10 digit phone number with appropriate characters");
 			return false;
 		}
-	}
-
-
-	else {
-		alert("message");
-		return false;
 	}
 	
 	if (messages.length > 0)
