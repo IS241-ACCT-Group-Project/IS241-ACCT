0d079e4410d75929f5dfae257bc941737938e787
whowells-IS241 <whowells@my.stlcc.edu>
2022-04-25 17:54:29 -0500

    added navbars to injectorHomePage and siteHomePage from home page

--- site/scripts/injectorHomePage.js
+++ site/scripts/injectorHomePage.js
@@ -8,7 +8,7 @@ function myFunction() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("injector-dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
