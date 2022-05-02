b041f12fed064814ecd8d55205bb9252c0fad567
whowells-IS241 <whowells@my.stlcc.edu>
2022-05-02 15:08:29 -0500

    updated styles on injector home page, added Logout button

--- site/scripts/injectorHomePage.js
+++ site/scripts/injectorHomePage.js
@@ -2,18 +2,18 @@
 toggle between hiding and showing the dropdown content */
 /* borrowed from https://www.w3schools.com/howto/howto_js_dropdown.asp */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("inj-show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.inj-dropbtn')) {
     var dropdowns = document.getElementsByClassName("injector-dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains('inj-show')) {
+        openDropdown.classList.remove('inj-show');
       }
     }
   }
